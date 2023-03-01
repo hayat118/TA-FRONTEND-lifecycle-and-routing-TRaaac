02f3404576e6abc305e7454c7bc200221d2b2b22
Tarique Hayat <tariqueht6@gmail.com>
2023-03-01 18:04:37 +0530

    Quiz App

--- block-BRaacg/quiz-app/src/component/start.js
+++ block-BRaacg/quiz-app/src/component/start.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../styles/index.css";
-// import Quiz from "./quiz";
+import { withRouter } from "../utils/withRouter";
 
 class Start extends React.Component {
   constructor(props) {
@@ -9,6 +9,8 @@ class Start extends React.Component {
     this.state = {
       data: null,
       quiz: "",
+      difficulty: "easy",
+      category: "11",
     };
   }
 
@@ -16,14 +18,22 @@ class Start extends React.Component {
     fetch(`https://opentdb.com/api_category.php`)
       .then((res) => res.json())
       .then((data) => this.setState({ data }));
-    // .then((data) => console.log(data, "data"));
   }
 
-  // handleClick = () => {
-  //   this.setState({
-  //     quiz: <Route path="/quiz" element={<Quiz />} />,
-  //   });
-  // };
+  handleCategory = (event) => {
+    console.log(event.target.value, "cat");
+
+    this.setState({
+      category: event.target.value,
+    });
+  };
+
+  handleDifficulty = (e) => {
+    console.log(e.target.value, "dif");
+    this.setState({
+      difficulty: e.target.value,
+    });
+  };
 
   render() {
     if (!this.state.data) {
@@ -31,37 +41,52 @@ class Start extends React.Component {
     }
     return (
       <>
-        <section className="start-box">
-          <h1>Quiz App</h1>
-          <div>
-            <label htmlFor="dificulty">Choose Dificulty:</label>
-            <select name="dificulty" id="dificulty">
-              {this.state.data.trivia_categories.map((post, i) => {
-                return (
-                  <option key={post.id}>
-                    {this.state.data.trivia_categories[i].name}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <div>
-            <label htmlFor="category">Choose Category:</label>
-            <select name="category" id="category">
-              <option>Easy</option>
-              <option>Mediuum</option>
-              <option>Hard</option>
-            </select>
-          </div>
-          <div>
-            <NavLink className="start" to="/quiz">
-              START
-            </NavLink>
-          </div>
-        </section>
+        <div className="box">
+          <section className="start-box">
+            <h1>Quiz App</h1>
+            <div>
+              <label className="label" htmlFor="category">
+                Choose Category :
+              </label>
+              <select
+                onChange={this.handleCategory}
+                name="category"
+                id="category"
+              >
+                {this.state.data.trivia_categories.map((category, i) => {
+                  return (
+                    <option key={category.id} value={category.id}>
+                      {category.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div>
+              <label htmlFor="difficulty">Choose Difficulty:</label>
+              <select
+                onChange={this.handleDifficulty}
+                name="difficulty"
+                id="difficulty"
+              >
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+              </select>
+            </div>
+            <div>
+              <NavLink
+                className="start"
+                to={`/quiz?difficulty=${this.state.difficulty}&category=${this.state.category}`}
+              >
+                START
+              </NavLink>
+            </div>
+          </section>
+        </div>
       </>
     );
   }
 }
 
-export default Start;
+export default withRouter(Start);
--- block-BRaacg/quiz-app/src/index.js
+++ block-BRaacg/quiz-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./component/app";
+import App from "./component/App";
 import { BrowserRouter } from "react-router-dom";
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
